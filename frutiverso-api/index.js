2df356adb34d814021146a2584564ef1a01a4921
Indi0101 <zindira1994@outlook.com>
2026-09-09 19:43:32 -0600

    commit

--- frutiverso-api/index.js
+++ frutiverso-api/index.js
@@ -8,11 +8,11 @@ app.use(cors());
 app.use(express.json());
 
 //conexion a mysql
-const db =mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "frutiverso"
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 db.connect((err)=>{
